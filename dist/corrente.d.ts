170aba1fbaa1262fe95be78385493bd7a9e9386b
Guilherme Caetano <guilherme.caetanostz@gmail.com>
2025-09-11 16:43:58 -0300

    atividade

--- dist/corrente.d.ts
+++
@@ -1,8 +0,0 @@
-import Conta from './conta.js';
-declare class Corrente extends Conta {
-    private taxaManutencao;
-    constructor(titular: string, saldo: number, taxaManutencao: number);
-    cobrarTaxa(): void;
-}
-export default Corrente;
-//# sourceMappingURL=corrente.d.ts.map
--- dist/poupanca.d.ts
+++
@@ -1,8 +0,0 @@
-import Conta from "./conta.js";
-declare class Poupanca extends Conta {
-    private taxaRendimento;
-    constructor(titular: string, saldo: number, taxaRendimento: number);
-    renderJuros(): void;
-}
-export default Poupanca;
-//# sourceMappingURL=poupanca.d.ts.map
